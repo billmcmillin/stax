2da39af50ca285f7afdd929bc98bd48ed0d74458
Bill McMillin <billmcmillin@gmail.com>
2015-01-04 18:56:48 -0500

    updated goal example file

--- mapFuncs_Goal.php
+++ mapFuncs_Goal.php
@@ -1,23 +1,109 @@
 <?php
-function aStacksMap($callNum){
-    
-	$location = "a";
-	$callNum = strip_tags($callNum);    
-	$subCall = substr($callNum, 0, 20);    
-	print "<p>You searched for $callNum</p>";if a ab.jpgfunction bStacksMap($callNum){
-    
-	$location = "b";
-	$callNum = strip_tags($callNum);    
-	$subCall = substr($callNum, 0, 20);    
-	print "<p>You searched for $callNum</p>";if b bc.jpgfunction cStacksMap($callNum){
-    
-	$location = "c";
-	$callNum = strip_tags($callNum);    
-	$subCall = substr($callNum, 0, 20);    
-	print "<p>You searched for $callNum</p>";if c cd.jpgfunction dStacksMap($callNum){
-    
-	$location = "d";
-	$callNum = strip_tags($callNum);    
-	$subCall = substr($callNum, 0, 20);    
-	print "<p>You searched for $callNum</p>";if d de.jpg
+/*mapFuncs_Goal.php
+This is an example of how the mapFuncs.php file should look when the
+generator program is run.
+*/
+
+//include function to sort callNums
+//include("../include/sortLC.php");
+
+class stdObject {
+    public function __construct(array $arguments = array()) {
+        if (!empty($arguments)) {
+            foreach ($arguments as $property => $argument) {
+                $this->{$property} = $argument;
+            }
+        }
+    }
+
+    public function __call($method, $arguments) {
+        $arguments = array_merge(array("stdObject" => $this), $arguments); // Note: method argument 0 will always referred to the main class ($this).
+        if (isset($this->{$method}) && is_callable($this->{$method})) {
+            return call_user_func_array($this->{$method}, $arguments);
+        } else {
+            throw new Exception("Fatal error: Call to undefined method stdObject::{$method}()");
+        }
+    }
+}
+
+//variables passed in from linx.js
+$callNum = htmlspecialchars($_GET["callNum"]);
+$loc = htmlspecialchars($_GET["loc"]);
+
+//$subCall = sortable($callNum);
+$subCall = $callNum;
+//result page that is displayed when user clicks on a link
+	print "<html>";
+	print "<head>";
+
+	print "<style>";
+	print "p {
+            font: 18px/20px verdana, sans-serif;
+            
+        }";
+        
+        print ".main {
+        	margin-left: auto; 
+            margin-right:auto;
+            width: 50%;
+            }";
+	print "</style>";
+	print "<body>";
+	print '<div class="main">';
+
+
+//declare an object for each location and populate it
+	
+	$locObj = new stdObject();
+	$locObj->libName = 'Milner';
+	$locObj->location = 'Floor 2 Stacks';
+	$locObj->callNumBegin = 'A';
+	$locObj->callNumEnd = 'M';
+	$locObj->imageFile = 'floor2range1.gif';
+
+	$locObj1 = new stdObject();
+	$locObj1->libName = 'Milner';
+	$locObj1->location = 'Floor 2 Stacks';
+	$locObj1->callNumBegin = 'N';
+	$locObj1->callNumEnd = 'Z';
+	$locObj1->imageFile = 'floor2range2.gif';
+
+//put all the objects in an array
+
+	$locArray = array(
+		0 => $locObj,
+		1 => $locObj1,
+		);
+	//var_dump($locArray);
+
+
+//begin checking for locations
+
+
+	foreach ($locArray as $locValue => $cur){
+
+		//iterate through each location object and if a match is found, display the map
+		if (($cur->location === $loc)&& ($subCall >= $cur->callNumBegin) && ($subCall < $cur->callNumEnd)) {
+			print '<p>Call number ' . $subCall . ' is in location: ' . $cur->location . '.';
+			print '<img src="../img/' . $cur->imageFile . '"/><br />';
+
+			//print "subcall " . $subCall . " is between " . ($cur->callNumBegin) . " and ";
+			//echo $cur->callNumEnd;
+		}
+		//if no match is found, display error message
+		else {
+			print '<p>Could not find this item. Please ask a librarian for assistance.</p>'
+		}
+
+	}
+
+
+
+//end location check
+
+	print "</div>";
+	print "</body>";
+	print "</html>";
+
+
 ?>
